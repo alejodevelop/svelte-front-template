e6ef7cc5516f09be6fa7a72efcdf0491dbf04ba2
alejo <alejodevelop@gmail.com>
2024-09-07 20:10:03 -0500

    FEATURE: navbar backbone created

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,3 @@
 // place files you want to import through the `$lib` alias in this folder.
 export { default as Login } from './login/login.svelte';
+export { default as Navbar } from './navbar/navbar.svelte';
---
+++ src/lib/navbar/navbar.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<div>
+	<nav>
+		<ul class="title">
+			<li><strong>@ALEJODEVELOP</strong></li>
+		</ul>
+		<ul class="links">
+			<li><a href="#" class="contrast">About</a></li>
+			<li><a href="#" class="contrast">Services</a></li>
+			<li><a href="#" class="contrast">Products</a></li>
+		</ul>
+	</nav>
+</div>
+
+<style>
+	.title {
+		margin-left: 2rem;
+	}
+
+	.links {
+		margin-right: 2rem;
+	}
+
+	nav {
+		border-bottom: 1px solid #ccc;
+	}
+</style>
